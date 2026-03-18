1fc11f63f97c36b076096eeeb36c7181f893b395
Timothe Sandt <timothe.sandt@univ-fcomte.fr>
2026-03-18 12:24:19 +0100

    Initial Commit

---
+++ SpotLight/SpotLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotLightApp.swift
+//  SpotLight
+//
+//  Created by sandt timothe on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpotLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
